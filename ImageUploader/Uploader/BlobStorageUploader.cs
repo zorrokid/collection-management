832095c0ea029cd2813ade40c9e9053195921951
Mikko Keinänen <mikko.keinanen@gmail.com>
2022-07-12 11:04:24 +0300

    Not necessary to use StringGroup

--- ImageUploader/Uploader/BlobStorageUploader.cs
+++ ImageUploader/Uploader/BlobStorageUploader.cs
@@ -1,8 +1,6 @@
 using Azure.Storage;
 using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
-using ImageUploader.StringUtils;
-
 namespace ImageUploader.Uploader;
 
 public interface IBlobStorageUploader
@@ -24,34 +22,30 @@ public class BlobStorageUploader : IBlobStorageUploader
   public async Task Upload(string directoryPath, string containerName, CancellationToken cancellationToken = default)
   {
     var containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-
-    // Grouping files by product:
-    // filename.1.jpg
-    // filename.2.jpg
-    // ...
-    // filename.n.jpg
-
     var files = Directory.EnumerateFiles(directoryPath)
       .Select(f => Path.GetFileName(f));
-    var groupedFiles = new StringGroup(files).GroupByPrefix(".");
 
-    foreach (var key in groupedFiles.Keys)
+    foreach (var fileName in files)
     {
-      foreach (var fileName in groupedFiles[key])
+      var blobClient = containerClient.GetBlobClient(fileName);
+      var fileStream = File.OpenRead(Path.Combine(directoryPath, fileName));
+
+      // Grouping files by file prefix:
+      // filename.1.jpg
+      // filename.2.jpg
+      // ...
+      // filename.n.jpg
+      var fileGroupKey = fileName.Split(".")[0];
+      var uploadOptions = new BlobUploadOptions
       {
-        var blobClient = containerClient.GetBlobClient(fileName);
-        var fileStream = File.OpenRead(Path.Combine(directoryPath, fileName));
-        var uploadOptions = new BlobUploadOptions
-        {
-          Tags = new Dictionary<string, string>
+        Tags = new Dictionary<string, string>
           {
-            { "fileGroup", key }
+            { "fileGroup", fileGroupKey }
           }
-        };
-        Console.WriteLine($"Start uploading file {fileName} with fileGroup-tag {key}");
-        await blobClient.UploadAsync(fileStream, uploadOptions, cancellationToken);
-        Console.WriteLine($"Finish uploading file {fileName} with fileGroup-tag {key}");
-      }
+      };
+      Console.WriteLine($"Start uploading file {fileName} with fileGroup-tag {fileGroupKey}");
+      await blobClient.UploadAsync(fileStream, uploadOptions, cancellationToken);
+      Console.WriteLine($"Finish uploading file {fileName} with fileGroup-tag {fileGroupKey}");
     }
   }
 }
